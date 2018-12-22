0f7a8d71df673db292acadfafb28f49542bb9d20
oramoetaro <oramoetaro@gmail.com>
2018-12-22 14:07:37 -0600

    axes added

--- chart.js
+++ chart.js
@@ -1,11 +1,8 @@
 (function () {
-
-  $('[data-toggle="tooltip"]').tooltip();
-
   const url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json';
   const w = $('#chart').width();
   const h = 300;
-  const padding = 0;
+  const padding = 40;
 
   var req = new XMLHttpRequest();
   req.open('GET', url, true);
@@ -21,10 +18,11 @@
       ]).range([padding, w - padding]);
 
     const yScale = d3.scaleLinear()
-      .domain([
-        d3.min(dataset, (d) => d[1]),
-        d3.max(dataset, (d) => d[1])
-      ]).range([padding, h - padding]);
+      .domain([ 0, d3.max(dataset, (d) => d[1])])
+      .range([padding, h - padding]);
+
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale);
 
     const svg = d3.select(".chart")
       .append("svg")
@@ -32,22 +30,34 @@
       .attr("height", h);
 
     svg.selectAll("rect")
-    .data(dataset)
-    .enter()
-    .append("rect")
-    .attr("class", "bar")
-    .attr("data-date", (d) => d[0])
-    .attr("data-gdp", (d) => d[1])
-    .attr("x", (d) => xScale(new Date(d[0]).getTime()))
-    .attr("y", (d) => h - yScale(d[1]))
-    .attr("width", w / dataset.length)
-    .attr("height", (d) => yScale(d[1]))
-    .attr("fill", "#c94c4c")
-    .append("title")
-    .text((d) => d[0]);
+      .data(dataset)
+      .enter()
+      .append("rect")
+      .attr("class", "bar")
+      .attr("data-date", (d) => d[0])
+      .attr("data-gdp", (d) => d[1])
+      .attr("x", (d) => xScale(new Date(d[0]).getTime()))
+      .attr("y", (d) => h - yScale(d[1]))
+      .attr("width", w / dataset.length)
+      .attr("height", (d) => yScale(d[1]) - padding)
+      .attr("fill", "#c94c4c")
+      .attr("data-toggle", "tooltip")
+      .attr("data-placement", "right")
+      .attr("title", "titulo");
 
-    // document.getElementById('char-wrapper')
-    //   .innerHTML = JSON.stringify(dataset);
+    svg.append("g")
+      .attr("id", "x-axis")
+      .attr("transform", "translate(0, " + (h - padding) + ")")
+      .call(xAxis);
+
+      svg.append("g")
+      .attr("id", "y-axis")
+      .attr("transform", "translate(" + padding + ", 0)")
+      .call(yAxis);
+
+    // Initialize Bootstrap Tooltips
+    $('[data-toggle="tooltip"]').tooltip()
+
   };
 
 })();
