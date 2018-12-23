b42a388e2b507b89084d883f26e92574b3f2a683
oramoetaro <oramoetaro@gmail.com>
2018-12-22 20:36:19 -0600

    y scale refactored.

--- chart.js
+++ chart.js
@@ -19,13 +19,12 @@
 
     const yScale = d3.scaleLinear()
       .domain([0, d3.max(dataset, (d) => d[1])])
-      .range([padding, h - padding]);
+      .range([h - padding, padding]);
 
     const xAxis = d3.axisBottom(xScale);
     const yAxis = d3.axisLeft(yScale);
 
     xAxis.ticks(6);
-    yAxis.tickValues([yScale(0), yScale(15000)]);
 
     const svg = d3.select(".chart")
       .append("svg")
@@ -40,9 +39,9 @@
       .attr("data-date", (d) => d[0])
       .attr("data-gdp", (d) => d[1])
       .attr("x", (d) => xScale(new Date(d[0])))
-      .attr("y", (d) => h - yScale(d[1]))
+      .attr("y", (d) => yScale(d[1]))
       .attr("width", w / dataset.length)
-      .attr("height", (d) => yScale(d[1]) - padding)
+      .attr("height", (d) => h - yScale(d[1]) - padding)
       .attr("fill", "#c94c4c")
       .attr("data-toggle", "tooltip")
       .attr("data-placement", "right")
